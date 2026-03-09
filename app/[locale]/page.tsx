c74e77f0f903179243ee53d1c9ae7d6b62093a0c
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-09 23:53:17 +0100

    add basic homepage text

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,19 +1,43 @@
-import { useTranslations } from "next-intl";
+import { useFormatter, useTranslations } from "next-intl";
 import { Link } from "@/i18n/navigation";
 
 export default function Home() {
   const t = useTranslations("home");
+  const format = useFormatter();
 
   return (
     <div>
-      <hgroup>
-        <h1>{t("title")}</h1>
-        <p>{t("description")}</p>
+      <section>
+        <hgroup>
+          <h1>{t("intro.title")}</h1>
+          <p>{t("intro.desc")}</p>
+          <p>
+            {t.rich("intro.desc2", {
+              contact: (chunks) => (
+                <Link href="/contact" className="underline">
+                  {chunks}
+                </Link>
+              ),
+            })}
+          </p>
+        </hgroup>
+
+        <p>{t("intro.gameDesc")}</p>
         <p>
-          {t.rich("description2", {
-            github: (chunks) => (
+          {t.rich("intro.gameDesc2", {
+            website: (chunks) => (
               <a
-                href="https://github.com"
+                href="https://https://www.bigambitionsgame.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline"
+              >
+                {chunks}
+              </a>
+            ),
+            steam: (chunks) => (
+              <a
+                href="https://store.steampowered.com/app/1331550/Big_Ambitions/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="underline"
@@ -23,7 +47,44 @@ export default function Home() {
             ),
           })}
         </p>
-      </hgroup>
+      </section>
+
+      <section>
+        <h2>{t("cta.title")}</h2>
+        <p>
+          {t.rich("cta.desc", {
+            i: (chunks) => <i>{chunks}</i>,
+            database: (chunks) => (
+              <Link href="/database" className="underline">
+                {chunks}
+              </Link>
+            ),
+          })}
+        </p>
+        <p>
+          {t.rich("cta.desc2", {
+            tools: (chunks) => (
+              <Link href="/tools" className="underline">
+                {chunks}
+              </Link>
+            ),
+          })}
+        </p>
+      </section>
+
+      <section>
+        <h2>{t("updates.title")}</h2>
+        <p>
+          <strong>
+            {format.dateTime(new Date("2026-03-09"), {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+            })}
+          </strong>{" "}
+          - {t("updates.update1")}
+        </p>
+      </section>
 
       <Link locale="en" href="/" className="underline">
         English
