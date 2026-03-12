e8a74d55ecbeb81c7c6eb5c40aeca330c73ebd82
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-12 16:31:03 +0100

    add types for database tables

--- lib/game/types.ts
+++ lib/game/types.ts
@@ -1,5 +1,21 @@
+import { IngredientName } from "./ingredientNames";
+import { MachineName } from "./machineNames";
+import { ProductName } from "./productNames";
+
 export type Difficulty = "easy" | "normal" | "hard";
 
 export type Price = {
   [K in Difficulty]: number;
 };
+
+type TableKeyMap = {
+  products: ProductName;
+  ingredients: IngredientName;
+  machines: MachineName;
+};
+
+export type ItemName = ProductName | IngredientName | MachineName;
+
+export type TableType = keyof TableKeyMap;
+
+export type TableData<T extends TableType, V> = [TableKeyMap[T], V][];
