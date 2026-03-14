ebe33141c6d86fcbc2f627deccc824b8fd701dca
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-14 16:16:45 +0100

    refactor boilerplate column array

--- app/[locale]/database/inventory/table-columns.tsx
+++ app/[locale]/database/inventory/table-columns.tsx
@@ -2,70 +2,18 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { Shelf } from "@/lib/game/inventory";
-import { ArrowUp } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { getMeta } from "@/lib/utils/getMeta";
+import {
+  createCurrencyColumn,
+  createNumericColumn,
+  createTranslatedColumn,
+} from "@/components/tables/shared-table-columns";
 
-export const columns: ColumnDef<Shelf>[] = [
-  {
-    accessorKey: "itemName",
-    header: ({ column, table }) => {
-      const { t } = getMeta(table);
-      const isSorted = column.getIsSorted();
-      return (
-        <Button
-          variant="ghost"
-          className="flex w-full flex-row justify-start px-2 py-0 text-left"
-          onClick={() => column.toggleSorting(isSorted === "asc")}
-        >
-          {t("database.table.inventory.headers.itemName")}
-          {isSorted && (
-            <ArrowUp className={isSorted === "asc" ? "" : "rotate-180"} />
-          )}
-        </Button>
-      );
-    },
-    cell: ({ row, table }) => {
-      const { t } = getMeta(table);
+type InventoryColumnData = Shelf & {
+  itemName: string;
+};
 
-      const value = row.getValue("itemName") as string;
-      return t(`inventory.${value}`);
-    },
-  },
-  {
-    accessorKey: "purchasePrice",
-    header: ({ column }) => {
-      const isSorted = column.getIsSorted();
-      return (
-        <Button
-          variant="ghost"
-          className="flex w-full flex-row justify-start px-2 py-0 text-left"
-          onClick={() => column.toggleSorting(isSorted === "asc")}
-        >
-          Price
-          {isSorted && (
-            <ArrowUp className={isSorted === "asc" ? "" : "rotate-180"} />
-          )}
-        </Button>
-      );
-    },
-  },
-  {
-    accessorKey: "storageCapacity",
-    header: ({ column }) => {
-      const isSorted = column.getIsSorted();
-      return (
-        <Button
-          variant="ghost"
-          className="flex w-full flex-row justify-start px-2 py-0 text-left"
-          onClick={() => column.toggleSorting(isSorted === "asc")}
-        >
-          Storage Capacity
-          {isSorted && (
-            <ArrowUp className={isSorted === "asc" ? "" : "rotate-180"} />
-          )}
-        </Button>
-      );
-    },
-  },
+export const columns: ColumnDef<InventoryColumnData>[] = [
+  createTranslatedColumn("itemName", "inventory"),
+  createCurrencyColumn("purchasePrice"),
+  createNumericColumn("storageCapacity"),
 ];
