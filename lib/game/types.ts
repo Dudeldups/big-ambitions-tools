1f18fdc41800f76ab57c61d9ac0601ea3f570402
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-14 18:27:11 +0100

    add difficulty for table meta

--- lib/game/types.ts
+++ lib/game/types.ts
@@ -10,6 +10,7 @@ declare module "@tanstack/react-table" {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface TableMeta<TData extends RowData> {
     t: Translator;
+    difficulty: Difficulty;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
