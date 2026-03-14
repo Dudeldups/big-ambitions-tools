d02662353d94d3821e5dd79e12d963ad4eb33e54
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-14 15:10:49 +0100

    add component for table headers

---
+++ components/tables/table-header-content.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { ArrowDownWideNarrow, ArrowUpNarrowWide } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Column } from "@tanstack/react-table";
+
+type Props<TData, TValue> = {
+  column: Column<TData, TValue>;
+  align?: "start" | "center" | "end";
+  children: React.ReactNode;
+};
+
+const TableHeaderContent = <TData, TValue>({
+  column,
+  align = "start",
+  children,
+}: Props<TData, TValue>) => {
+  const isSorted = column.getIsSorted();
+  const canSort = column.getCanSort();
+
+  if (!canSort) {
+    return (
+      <span className={`flex justify-${align} w-full px-2 py-0`}>
+        {children}
+      </span>
+    );
+  }
+
+  return (
+    <Button
+      variant="ghost"
+      className={`flex w-full justify-${align} px-2 py-0`}
+      onClick={() => column.toggleSorting(isSorted === "asc")}
+    >
+      {children}
+      {isSorted === "asc" ? (
+        <ArrowUpNarrowWide />
+      ) : isSorted === "desc" ? (
+        <ArrowDownWideNarrow />
+      ) : null}
+    </Button>
+  );
+};
+
+export default TableHeaderContent;
