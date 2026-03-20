9fef4095b477021c89de8ca7fceac3b7f0e1076f
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-20 16:17:47 +0100

    add conditional rendering for overviews

--- app/[locale]/tools/page.tsx
+++ app/[locale]/tools/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import { useTranslations } from "next-intl";
 import PlaythroughOverview from "./playthrough-overview";
+import FactoryOverview from "./factory-overview";
+import { usePlaythroughState } from "@/lib/hooks/usePlaythroughState";
+import PlaythroughDashboard from "./playthrough-dashboard";
+import { Spinner } from "@/components/ui/spinner";
 
 const Tools = () => {
   const t = useTranslations("tools");
+  const activePlaythroughId = usePlaythroughState(
+    (state) => state.activePlaythroughId,
+  );
 
   return (
     <>
@@ -12,7 +21,16 @@ const Tools = () => {
           <p>{t("intro.desc")}</p>
         </hgroup>
 
-        <PlaythroughOverview />
+        {activePlaythroughId === undefined ? (
+          <Spinner />
+        ) : activePlaythroughId ? (
+          <PlaythroughDashboard />
+        ) : (
+          <>
+            <PlaythroughOverview />
+            <FactoryOverview />
+          </>
+        )}
       </section>
     </>
   );
