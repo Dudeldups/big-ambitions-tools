8b42b1f269ce39f510f925cd84aa0244885d0b16
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-25 21:10:47 +0100

    add price multis

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,3 +5,17 @@ export const DIFFICULTY_OPTIONS = [
   "normal",
   "hard",
 ] as const satisfies readonly Difficulty[];
+
+export const IMPORT_PRICE_BASE_MULT = 0.75;
+
+export const PUBLIC_PRICE_MULT = {
+  easy: 0.7,
+  normal: 0.7,
+  hard: 1.3,
+} satisfies Record<Difficulty, number>;
+
+export const EXPORT_PRICE_MULT = {
+  easy: 0.8,
+  normal: 0.65,
+  hard: 0.5,
+} satisfies Record<Difficulty, number>;
