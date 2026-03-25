cdfb5bb7773d4b18c3147a6148f52c6332ec5f06
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-25 23:05:10 +0100

    refactor currency converter with spinner

---
+++ lib/utils/currencyCell.tsx
@@ -0,0 +1,26 @@
+import { formatToUSD } from "./formatToUSD";
+import { CellContext } from "@tanstack/react-table";
+import { getMeta } from "./getMeta";
+import { StoreDifficulty } from "../game/types";
+import { Spinner } from "@/components/ui/spinner";
+
+export function currencyCell<TData>(
+  getValueFn: (row: TData, difficulty?: StoreDifficulty) => number | undefined,
+) {
+  return function CurrencyCell(ctx: CellContext<TData, unknown>) {
+    const { difficulty } = getMeta(ctx.table);
+    const row = ctx.row.original;
+
+    const value = getValueFn(row, difficulty);
+
+    if (!value) {
+      return (
+        <div className="flex h-4 justify-end">
+          <Spinner />
+        </div>
+      );
+    }
+
+    return formatToUSD(value);
+  };
+}
