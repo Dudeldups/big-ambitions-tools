f91432ac803d0e138724fe9a481710aa9f4762b7
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-26 23:44:30 +0100

    add factory info cards

--- app/[locale]/tools/[playthroughId]/page.tsx
+++ app/[locale]/tools/[playthroughId]/page.tsx
@@ -1,19 +1,14 @@
 "use client";
 
+import FactoryInfoCard from "@/components/tools/factory-info-card";
 import { Button } from "@/components/ui/button";
 import { Link } from "@/i18n/navigation";
 import { useActivePlaythrough } from "@/lib/hooks/useActivePlaythrough";
-import { usePlaythroughState } from "@/lib/hooks/usePlaythroughState";
 import { Plus } from "lucide-react";
 
 const PlaythroughId = () => {
+  // const t = useTranslations();
   const { activePlaythrough } = useActivePlaythrough();
-
-  const factories = usePlaythroughState((s) => s.factories);
-
-  if (!activePlaythrough) {
-    return null;
-  }
 
   const hasFactories = activePlaythrough.factoryIds.length !== 0;
 
@@ -49,11 +44,14 @@ const PlaythroughId = () => {
             </Link>
           </>
         ) : (
-          <ul>
-            {activePlaythrough?.factoryIds.map((factoryId) => (
-              <li key={factoryId}>
-                <p>{factoryId}</p>
-                <p>{factories?.find((f) => f.id === factoryId)?.name}</p>
+          <ul className="flex items-stretch gap-4">
+            {activePlaythrough.factoryIds.map((factoryId) => (
+              <li key={factoryId} className="mt-4">
+                <Link
+                  href={`/tools/${activePlaythrough.id}/factories/${factoryId}`}
+                >
+                  <FactoryInfoCard factoryId={factoryId} />
+                </Link>
               </li>
             ))}
           </ul>
---
+++ components/tools/factory-info-card.tsx
@@ -0,0 +1,50 @@
+import { usePlaythroughState } from "@/lib/hooks/usePlaythroughState";
+import { useTranslations } from "next-intl";
+import { Card, CardContent, CardHeader } from "../ui/card";
+
+type FactoryInfoCardProps = {
+  factoryId: string;
+};
+
+const FactoryInfoCard = ({ factoryId }: FactoryInfoCardProps) => {
+  const t = useTranslations();
+  const factory = usePlaythroughState((s) => s.getFactoryById(factoryId));
+
+  if (!factory) return null;
+
+  const groupedWorkstations = Object.values(
+    factory.workstations.reduce(
+      (acc, ws) => {
+        if (!acc[ws.name]) {
+          acc[ws.name] = { name: ws.name, count: 0 };
+        }
+
+        acc[ws.name].count += 1;
+
+        return acc;
+      },
+      {} as Record<string, { name: string; count: number }>,
+    ),
+  );
+
+  return (
+    <Card className="flex h-full flex-col">
+      <CardHeader>
+        <h3>{factory.name}</h3>
+        {factory.description && <p>{factory.description}</p>}
+      </CardHeader>
+      <CardContent className="mt-auto">
+        <p>Opening hours / week: {factory.openingHours * 7}h</p>
+        <ul>
+          {groupedWorkstations.map((workstation) => (
+            <li key={workstation.name}>
+              {workstation.count}x {t(`workstations.${workstation.name}`)}
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default FactoryInfoCard;
