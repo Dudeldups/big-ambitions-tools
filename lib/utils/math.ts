4cd4584b8b17c748cb1c61d2b3b14767483ff53a
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-26 10:10:07 +0100

    add priceIndex to getter functions

--- lib/utils/math.ts
+++ lib/utils/math.ts
@@ -5,19 +5,25 @@ import { Difficulty } from "../game/types";
 export const getImportPrice = (
   wholesalePrice: number,
   difficulty: Difficulty,
+  priceIndex: number = 1,
 ) => {
   return (
-    wholesalePrice * IMPORT_PRICE_BASE_MULT * PUBLIC_PRICE_MULT[difficulty]
+    wholesalePrice *
+    IMPORT_PRICE_BASE_MULT *
+    PUBLIC_PRICE_MULT[difficulty] *
+    priceIndex
   );
 };
 
 export const getExportPrice = (
   wholesalePrice: number,
   difficulty: Difficulty,
+  priceIndex: number = 1,
 ) => {
   return (
     wholesalePrice *
     PUBLIC_PRICE_MULT[difficulty] *
-    EXPORT_PRICE_MULT[difficulty]
+    EXPORT_PRICE_MULT[difficulty] *
+    priceIndex
   );
 };
