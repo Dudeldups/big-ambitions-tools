ae47beadc078de6a5763f2be2ec1fe11d7f25ab1
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-28 21:19:07 +0100

    add boolean for hiding cents

--- lib/utils/formatToUSD.ts
+++ lib/utils/formatToUSD.ts
@@ -1,6 +1,24 @@
 export const usdFormatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
 });
 
-export const formatToUSD = (value: number) => usdFormatter.format(value);
+/**
+ * Format a number to USD.
+ * @param value The number to format
+ * @param hideCents Optional: if true, remove cents
+ */
+
+export const formatToUSD = (value: number, hideCents = false) => {
+  if (hideCents) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    }).format(value);
+  }
+  return usdFormatter.format(value);
+};
