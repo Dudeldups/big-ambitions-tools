cdf7d5b85c9a1d93738a8e523ad00233a7306aa8
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-28 22:47:08 +0100

    add deriveVehicleCost

--- lib/calculations/derivedFactoryData.ts
+++ lib/calculations/derivedFactoryData.ts
@@ -1,4 +1,5 @@
 import { workstations } from "../game/machines";
+import { vehicles } from "../game/vehicles";
 import { getWorkstationPrice } from "./math";
 
 export const deriveWorkstationCost = (wsName: string | undefined): number => {
@@ -6,4 +7,11 @@ export const deriveWorkstationCost = (wsName: string | undefined): number => {
   const workstation = workstations[wsName as keyof typeof workstations];
   if (!workstation) return 0;
   return getWorkstationPrice(workstation);
+};
+
+export const deriveVehicleCost = (vehicleName: string | undefined): number => {
+  if (!vehicleName) return 0;
+  const vehicle = vehicles[vehicleName as keyof typeof vehicles];
+  if (!vehicle) return 0;
+  return vehicle.purchasePrice;
 };
