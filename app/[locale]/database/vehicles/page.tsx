b664d04fb358550a24e1b5b01d823513886078ab
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-28 18:27:37 +0100

    add vehicle table

---
+++ app/[locale]/database/vehicles/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { DataTable } from "@/components/tables/data-table";
+import { VehicleName } from "@/lib/game/vehicleNames";
+import { vehicles } from "@/lib/game/vehicles";
+import { VehiclesColumnData, vehiclesColumns } from "./table-columns";
+
+const data: VehiclesColumnData[] = (Object.keys(vehicles) as VehicleName[]).map(
+  (vehicleName) => ({
+    ...vehicles[vehicleName],
+    vehicleName,
+  }),
+);
+
+export default function VehiclesPage() {
+  return (
+    <div className="mx-auto py-10">
+      <DataTable columns={vehiclesColumns} data={data} />
+    </div>
+  );
+}
---
+++ app/[locale]/database/vehicles/table-columns.tsx
@@ -0,0 +1,48 @@
+import {
+  createBooleanColumn,
+  createCurrencyColumn,
+  createNumericColumn,
+  createTranslatedColumn,
+} from "@/components/tables/shared-table-columns";
+import TableHeadContent from "@/components/tables/table-head-content";
+import { VehicleName } from "@/lib/game/vehicleNames";
+import { Vehicle } from "@/lib/game/vehicles";
+import { getMeta } from "@/lib/utils/getMeta";
+import { ColumnDef } from "@tanstack/react-table";
+
+export type VehiclesColumnData = Vehicle & {
+  vehicleName: VehicleName;
+};
+
+export const vehiclesColumns: ColumnDef<VehiclesColumnData>[] = [
+  createTranslatedColumn("vehicleName", "vehicles"),
+  createNumericColumn("maxSpeed"),
+  createNumericColumn("enginePower"),
+  createNumericColumn("maxFuel"),
+  createNumericColumn("maxCargoCapacity"),
+  createNumericColumn("requiredDeliveryDriverSkillValue"),
+  createNumericColumn("destinationsThatCanDeliver"),
+  {
+    id: "destinationsFull",
+    accessorFn: (row) => row.destinationsThatCanDeliver * 2,
+    header: ({ column, table }) => {
+      const { t } = getMeta(table);
+
+      return (
+        <TableHeadContent column={column} align="end">
+          {t("general.tableColumns.destinationsFull")}
+        </TableHeadContent>
+      );
+    },
+    cell: ({ getValue }) => {
+      const value = getValue() as number;
+      return value ?? "-";
+    },
+    meta: {
+      align: "right",
+    },
+  },
+  createCurrencyColumn("purchasePrice"),
+  createBooleanColumn("autoParkSupported"),
+  createBooleanColumn("taxDeductible"),
+];
