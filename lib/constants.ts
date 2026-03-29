4ef07be6dc5a9b6834e4909df7bf430a7defcab3
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-29 11:42:01 +0200

    add salary mults

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,6 +4,14 @@ export const NONE_VALUE = "__none__" as const;
 
 export const EMPLOYEE_MAX_SALARY = 1000 as const;
 
+export const SALARY_BASE_MULT = 2.25 as const;
+
+export const SALARY_DIFF_MULT = {
+  easy: 0.5,
+  normal: 0.7,
+  hard: 1,
+} as const satisfies Record<Difficulty, number>;
+
 export const DIFFICULTY_OPTIONS = [
   "easy",
   "normal",
