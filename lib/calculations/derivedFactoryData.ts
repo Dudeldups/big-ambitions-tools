541760228ed9e32f4fcea147abca3483ba8da497
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-03-29 15:42:40 +0200

    add deriveEmployeeCost

--- lib/calculations/derivedFactoryData.ts
+++ lib/calculations/derivedFactoryData.ts
@@ -1,17 +1,39 @@
+import { EmployeeName } from "../game/employeeNames";
+import { employees } from "../game/employees";
+import { WorkstationName } from "../game/machineNames";
 import { workstations } from "../game/machines";
+import { DeepPartial } from "../game/types";
+import { VehicleName } from "../game/vehicleNames";
 import { vehicles } from "../game/vehicles";
+import { FactoryFormValues } from "../schemas/factory";
 import { getWorkstationPrice } from "./math";
 
-export const deriveWorkstationCost = (wsName: string | undefined): number => {
+export const deriveWorkstationCost = (
+  wsName: WorkstationName | undefined,
+): number => {
   if (!wsName) return 0;
-  const workstation = workstations[wsName as keyof typeof workstations];
-  if (!workstation) return 0;
-  return getWorkstationPrice(workstation);
+  return getWorkstationPrice(workstations[wsName]);
 };
 
-export const deriveVehicleCost = (vehicleName: string | undefined): number => {
+export const deriveVehicleCost = (
+  vehicleName: VehicleName | undefined,
+): number => {
   if (!vehicleName) return 0;
-  const vehicle = vehicles[vehicleName as keyof typeof vehicles];
-  if (!vehicle) return 0;
-  return vehicle.purchasePrice;
+  return vehicles[vehicleName].purchasePrice;
+};
+
+export const deriveEmployeeCost = (
+  employeeName: EmployeeName | undefined,
+  values: DeepPartial<FactoryFormValues>,
+): number => {
+  if (!employeeName || !values.openingHours) return 0;
+  const employee = employees[employeeName];
+  const formEmployee =
+    values.employees?.[employeeName as keyof FactoryFormValues["employees"]];
+  if (!formEmployee?.salary) return 0;
+  const workingHours =
+    "customWorkingHours" in employee
+      ? employee.customWorkingHours
+      : values.openingHours;
+  return formEmployee.salary * workingHours * 7;
 };
