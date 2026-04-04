08498186435c75bd603106d804de2ea911134e5b
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-04 13:27:04 +0200

    add testValues for form

---
+++ __tests__/test-values.ts
@@ -0,0 +1,44 @@
+import { FactoryFormValues } from "@/lib/schemas/factory";
+
+export const _testFactoryFormValues: FactoryFormValues = {
+  name: "Example Factory",
+  description: "This is a test factory.",
+  openingHours: 12,
+  deliveryPeriod: "daily",
+  employees: {
+    deliveryDriver: {
+      amount: 1,
+      salary: 50,
+    },
+    hrManager: {
+      amount: 0,
+      salary: 80,
+    },
+    logisticsManager: {
+      amount: 1,
+      salary: 80,
+    },
+    purchasingAgent: {
+      amount: 0,
+      salary: 80,
+    },
+    factoryWorker: {
+      amount: 20,
+      salary: 25,
+    },
+  },
+  vehicles: ["FreightTruckT1"],
+  workstations: [
+    {
+      amount: 3,
+      name: "clothingWorkstation",
+      product: "classicCheapMaleClothing",
+      salesAmount: 1000,
+    },
+    {
+      amount: 1,
+      name: "electronicsWorkstation",
+      product: "artyFishPhone",
+    },
+  ],
+};
