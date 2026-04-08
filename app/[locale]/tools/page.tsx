0f679eb0a7dd97da919050b22564d32404566249
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-08 17:49:47 +0200

    add top bar with create button

--- app/[locale]/tools/page.tsx
+++ app/[locale]/tools/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import CreatePlaythroughForm from "@/components/tools/create-playthrough-form";
 import PlaythroughOverview from "@/components/tools/playthrough-overview";
 import { useTranslations } from "next-intl";
 
@@ -9,6 +10,12 @@ const Tools = () => {
   return (
     <>
       <section className="max-w-page mx-auto w-full">
+        <div className="bg-background border-border border-b">
+          <div className="mx-auto flex flex-wrap items-center gap-x-6 gap-y-3 px-4 py-3 sm:px-6">
+            <CreatePlaythroughForm />
+          </div>
+        </div>
+
         <hgroup>
           <h1>{t("intro.title")}</h1>
           <p>{t("intro.desc")}</p>
