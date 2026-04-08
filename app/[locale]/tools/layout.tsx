c4fb6a7b8d26fdc89123ecee6091fedec97eb0e0
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-08 17:28:31 +0200

    remove wrapper div

--- app/[locale]/tools/layout.tsx
+++ app/[locale]/tools/layout.tsx
@@ -1,9 +1,5 @@
 const ToolsLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="px-4 py-8">
-      <section className="max-w-page mx-auto w-full">{children}</section>
-    </div>
-  );
+  return <div className="px-4 py-8">{children}</div>;
 };
 
 export default ToolsLayout;
