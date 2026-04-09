6068536bc996cb7124b0621c18bfec639041dbf3
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-09 15:41:17 +0200

    add factory list to /factories

--- app/[locale]/tools/[playthroughId]/factories/page.tsx
+++ app/[locale]/tools/[playthroughId]/factories/page.tsx
@@ -1,5 +1,54 @@
-const FactoryPage = () => {
-  return <div>FactoryPage</div>;
+"use client";
+
+import FactoryInfoCard from "@/components/tools/factory-info-card";
+import { Link } from "@/i18n/navigation";
+import { useActivePlaythrough } from "@/lib/hooks/useActivePlaythrough";
+import { Plus } from "lucide-react";
+
+const FactoriesPage = () => {
+  // const t = useTranslations();
+  const { activePlaythrough } = useActivePlaythrough();
+
+  // TODO add skeletons
+  if (!activePlaythrough) {
+    return null;
+  }
+
+  const hasFactories = activePlaythrough.factoryIds.length !== 0;
+
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Go back to the playthrough overview:</p>
+          <Link href="/tools">Playthroughs</Link>
+        </div>
+
+        <h2>Factories in this playthrough:</h2>
+
+        {!hasFactories ? (
+          <>
+            <p>No factories yet. Create one here:</p>
+            <Link href={`/tools/${activePlaythrough.id}/factories/create`}>
+              <Plus />
+            </Link>
+          </>
+        ) : (
+          <ul className="flex items-stretch gap-4">
+            {activePlaythrough.factoryIds.map((factoryId) => (
+              <li key={factoryId} className="mt-4">
+                <Link
+                  href={`/tools/${activePlaythrough.id}/factories/${factoryId}`}
+                >
+                  <FactoryInfoCard factoryId={factoryId} />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
 
-export default FactoryPage;
+export default FactoriesPage;
--- app/[locale]/tools/[playthroughId]/page.tsx
+++ app/[locale]/tools/[playthroughId]/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import FactoryInfoCard from "@/components/tools/factory-info-card";
 import { Link } from "@/i18n/navigation";
 import { useActivePlaythrough } from "@/lib/hooks/useActivePlaythrough";
-import { Plus } from "lucide-react";
+import { Factory } from "lucide-react";
 
-const PlaythroughId = () => {
+const PlaythroughIdPage = () => {
   // const t = useTranslations();
   const { activePlaythrough } = useActivePlaythrough();
 
@@ -13,8 +12,6 @@ const PlaythroughId = () => {
   if (!activePlaythrough) {
     return null;
   }
-
-  const hasFactories = activePlaythrough.factoryIds.length !== 0;
 
   return (
     <div>
@@ -24,31 +21,28 @@ const PlaythroughId = () => {
           <Link href="/tools">Playthroughs</Link>
         </div>
 
-        <h2>Factories in this playthrough:</h2>
+        <h2>
+          Your playthrough{" "}
+          <span className="font-semibold">
+            {activePlaythrough.characterName}
+          </span>
+        </h2>
 
-        {!hasFactories ? (
-          <>
-            <p>No factories yet. Create one here:</p>
-            <Link href={`/tools/${activePlaythrough.id}/factories/create`}>
-              <Plus />
-            </Link>
-          </>
-        ) : (
-          <ul className="flex items-stretch gap-4">
-            {activePlaythrough.factoryIds.map((factoryId) => (
-              <li key={factoryId} className="mt-4">
-                <Link
-                  href={`/tools/${activePlaythrough.id}/factories/${factoryId}`}
-                >
-                  <FactoryInfoCard factoryId={factoryId} />
-                </Link>
-              </li>
-            ))}
-          </ul>
-        )}
+        <p>
+          This section is still a work in progress. You can visit the factories
+          page to see an overview of your factories, keep track of items you
+          need to order for your production lines and more. The factory planner
+          also shows you how many pallet shelves and factory workers you will
+          need for an efficient production process.
+        </p>
+
+        <Link href={`/tools/${activePlaythrough.id}/factories`}>
+          <Factory />
+          Factories
+        </Link>
       </div>
     </div>
   );
 };
 
-export default PlaythroughId;
+export default PlaythroughIdPage;
