58ffada4454661df7bbbae266a53098df0ca42ce
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-12 23:09:02 +0200

    add shopping list to factory page

--- app/[locale]/tools/[playthroughId]/factories/[factoryId]/page.tsx
+++ app/[locale]/tools/[playthroughId]/factories/[factoryId]/page.tsx
@@ -1,25 +1,63 @@
 "use client";
 
+import ImporterTable from "@/components/tables/importer-table";
 import { Link } from "@/i18n/navigation";
+import { getShoppingList } from "@/lib/calculations/getShoppingList";
 import { useActiveFactory } from "@/lib/hooks/useActiveFactory";
 import { useActivePlaythrough } from "@/lib/hooks/useActivePlaythrough";
+import { useTranslations } from "next-intl";
 
 const FactoryIdPage = () => {
+  const t = useTranslations();
   const { activePlaythrough } = useActivePlaythrough();
   const { activeFactory } = useActiveFactory();
 
   // TODO add skeletons
   if (!activeFactory || !activePlaythrough) return null;
 
+  const workstationAmount = activeFactory.workstations.reduce(
+    (sum, ws) => sum + ws.amount,
+    0,
+  );
+
+  const shoppingListData = getShoppingList(
+    activeFactory,
+    activePlaythrough.difficulty,
+  );
+
   return (
-    <div>
-      FactoryIdPage
-      <pre>{JSON.stringify(activeFactory, null, 2)}</pre>
+    <div className="max-w-page mx-auto">
+      <div>
+        <h2>{activeFactory.name}</h2>
+        {activeFactory.description && <p>{activeFactory.description}</p>}
+
+        <dl>
+          <dt>Opening hours</dt>
+          <dd>{activeFactory.openingHours}</dd>
+
+          <dt>Delivery period</dt>
+          <dd>{activeFactory.deliveryPeriod}</dd>
+
+          <dt>Workstation amount</dt>
+          <dd>{workstationAmount}</dd>
+        </dl>
+      </div>
       <Link
         href={`/tools/${activePlaythrough.id}/factories/${activeFactory.id}/edit`}
       >
         Edit factory
       </Link>
+
+      <div>
+        <h2>Shopping list</h2>
+        <p>This is what your factory needs to run at full capacity.</p>
+
+        <div className="space-y-6">
+          {shoppingListData.map((group) => (
+            <ImporterTable key={group.importer} data={group} t={t} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ components/tables/importer-table.tsx
@@ -0,0 +1,86 @@
+import { Translator } from "@/lib/types";
+import CopyButton from "../copy-button";
+import { ImporterShoppingList } from "@/lib/calculations/getShoppingList";
+import { formatToUSD } from "@/lib/utils/formatToUSD";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+
+type ImporterTableProps = {
+  data: ImporterShoppingList;
+  t: Translator;
+};
+
+const ImporterTable = ({ data, t }: ImporterTableProps) => {
+  const { importer, items } = data;
+
+  const total = items.reduce((sum, item) => sum + item.value, 0);
+
+  return (
+    <div className="space-y-2">
+      {/* Importer title */}
+      <h3 className="text-lg font-semibold">{t(`importers.${importer}`)}</h3>
+
+      <div className="max-w-2xl rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="text-right">
+                {t(`tableColumns.amount`)}
+              </TableHead>
+              <TableHead>{t(`tableColumns.itemName`)}</TableHead>
+              <TableHead className="text-right">
+                {t(`tableColumns.purchasePrice`)}
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {items
+              .sort((a, b) =>
+                t(`ingredients.${a.name}`).localeCompare(
+                  t(`ingredients.${b.name}`),
+                ),
+              )
+              .map((item) => (
+                <TableRow key={item.name}>
+                  <TableCell className="text-right">
+                    <div className="flex items-center justify-end gap-2">
+                      <span className="amount">{item.amount}</span>
+
+                      <CopyButton value={item.amount} />
+                    </div>
+                  </TableCell>
+
+                  <TableCell>{t(`ingredients.${item.name}`)}</TableCell>
+
+                  <TableCell className="text-right">
+                    {formatToUSD(item.value)}
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={2} className="font-semibold">
+                {t("general.summedUpAmount")}
+              </TableCell>
+              <TableCell className="text-right font-semibold">
+                {formatToUSD(total)}
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default ImporterTable;
