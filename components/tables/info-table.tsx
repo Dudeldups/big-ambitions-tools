d4c428fa6e24b76b27e2d0a439ecdf243a501548
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-14 18:28:51 +0200

    fix table overflow-x

--- components/tables/info-table.tsx
+++ components/tables/info-table.tsx
@@ -27,75 +27,77 @@ const InfoTable = ({ label, rows }: InfoTableProps) => {
   const hasDiff = rows.some((item) => !!item.diff);
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="text-right">
-              {t(`tableColumns.amount`)}
-            </TableHead>
-            <TableHead>{t(`tableColumns.${label}`)}</TableHead>
-            {hasDiff && (
+    <div className="mx-auto max-w-3xl overflow-x-hidden">
+      <div className="w-full overflow-x-auto rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
               <TableHead className="text-right">
-                {t(`general.netProfit`)}
+                {t(`tableColumns.amount`)}
               </TableHead>
-            )}
-            <TableHead className="text-right">
-              {t(`tableColumns.purchasePrice`)}
-            </TableHead>
-          </TableRow>
-        </TableHeader>
+              <TableHead>{t(`tableColumns.${label}`)}</TableHead>
+              {hasDiff && (
+                <TableHead className="text-right">
+                  {t(`general.netProfit`)}
+                </TableHead>
+              )}
+              <TableHead className="text-right">
+                {t(`tableColumns.purchasePrice`)}
+              </TableHead>
+            </TableRow>
+          </TableHeader>
 
-        <TableBody>
-          {rows
-            .sort(
-              (a, b) =>
-                (a.valueType ?? "").localeCompare(b.valueType ?? "") ||
-                a.name.localeCompare(b.name),
-            )
-            .map((row, i) => {
-              const showDivider =
-                row.valueType && row.valueType !== rows[i - 1]?.valueType;
+          <TableBody>
+            {rows
+              .sort(
+                (a, b) =>
+                  (a.valueType ?? "").localeCompare(b.valueType ?? "") ||
+                  a.name.localeCompare(b.name),
+              )
+              .map((row, i) => {
+                const showDivider =
+                  row.valueType && row.valueType !== rows[i - 1]?.valueType;
 
-              return (
-                <Fragment key={"f-" + row.name + i}>
-                  {showDivider && (
-                    <TableRow key={`divider-${row.valueType}`}>
-                      <TableCell colSpan={row.diff ? 4 : 3}>
-                        <div className="text-muted-foreground flex items-center gap-2 text-xs">
-                          <div className="bg-border h-px flex-1" />
-                          <span>{row.valueType?.toUpperCase()}</span>
-                          <div className="bg-border h-px flex-1" />
-                        </div>
+                return (
+                  <Fragment key={"f-" + row.name + i}>
+                    {showDivider && (
+                      <TableRow key={`divider-${row.valueType}`}>
+                        <TableCell colSpan={row.diff ? 4 : 3}>
+                          <div className="text-muted-foreground flex items-center gap-2 text-xs">
+                            <div className="bg-border h-px flex-1" />
+                            <span>{row.valueType?.toUpperCase()}</span>
+                            <div className="bg-border h-px flex-1" />
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    )}
+                    <TableRow key={row.name + i}>
+                      <TableCell className="amount">{row.amount}</TableCell>
+                      <TableCell className="truncate">{t(row.name)}</TableCell>
+                      {row.diff && (
+                        <TableCell className="amount">
+                          <CurrencyText value={row.diff} />
+                        </TableCell>
+                      )}
+                      <TableCell className={cn("amount")}>
+                        <span>{formatToUSD(row.value)}</span>
                       </TableCell>
                     </TableRow>
-                  )}
-                  <TableRow key={row.name + i}>
-                    <TableCell className="amount">{row.amount}</TableCell>
-                    <TableCell>{t(row.name)}</TableCell>
-                    {row.diff && (
-                      <TableCell className="amount">
-                        <CurrencyText value={row.diff} />
-                      </TableCell>
-                    )}
-                    <TableCell className={cn("amount")}>
-                      <span>{formatToUSD(row.value)}</span>
-                    </TableCell>
-                  </TableRow>
-                </Fragment>
-              );
-            })}
-        </TableBody>
+                  </Fragment>
+                );
+              })}
+          </TableBody>
 
-        <TableFooter>
-          <TableRow>
-            <TableCell colSpan={hasDiff ? 3 : 2} className="font-semibold">
-              {t("general.summedUpAmount")}
-            </TableCell>
-            <TableCell className="amount">{formatToUSD(total)}</TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={hasDiff ? 3 : 2} className="font-semibold">
+                {t("general.summedUpAmount")}
+              </TableCell>
+              <TableCell className="amount">{formatToUSD(total)}</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
     </div>
   );
 };
