d47fba58da0a08d3fe92e899ddb0fb149f492168
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-17 00:04:12 +0200

    add hook for isMounted check as React 19 suggests

---
+++ lib/hooks/useIsMounted.ts
@@ -0,0 +1,10 @@
+import { useSyncExternalStore } from "react";
+
+const emptySubscribe = () => () => {};
+export function useIsMounted() {
+  return useSyncExternalStore(
+    emptySubscribe,
+    () => true,
+    () => false,
+  );
+}
