fdf141a76bfb1a7ffb95fbe88e2bb872498a6bde
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-19 00:53:52 +0200

    add custom details replacement

---
+++ components/details.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
+import { ChevronDown } from "lucide-react";
+
+type DetailsProps = {
+  children: React.ReactNode;
+  title: string;
+};
+
+const Details = ({ children, title }: DetailsProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="overflow-x-hidden border-b" aria-expanded={isOpen}>
+      <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="flex w-full items-center justify-between py-5 text-lg font-semibold"
+      >
+        {title}
+
+        <motion.span
+          animate={{ rotate: isOpen ? 180 : 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <ChevronDown className="size-6" />
+        </motion.span>
+      </button>
+
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.div
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: "auto", opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="overflow-hidden"
+          >
+            <div className="pb-4">{children}</div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default Details;
