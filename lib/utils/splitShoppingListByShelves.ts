33b3668ffd5b1abec2ba2d55086d68fa8da2feb1
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-18 17:19:56 +0200

    create splitShoppingListByShelves

---
+++ lib/utils/splitShoppingListByShelves.ts
@@ -0,0 +1,67 @@
+import { IngredientName } from "../game/ingredientNames";
+import { ingredients } from "../game/ingredients";
+import { ImporterShoppingList } from "./getShoppingList";
+
+type SplitShoppingList = {
+  factoryList: ImporterShoppingList[];
+  externalList: ImporterShoppingList[];
+};
+
+export const splitShoppingListByShelves = (
+  list: ImporterShoppingList[],
+  requiredShelves: number,
+  availableShelves: number,
+): SplitShoppingList => {
+  if (requiredShelves <= 0 || availableShelves >= requiredShelves) {
+    return { factoryList: list, externalList: [] };
+  }
+
+  const factoryRatio = availableShelves / requiredShelves;
+
+  const factoryList: ImporterShoppingList[] = [];
+  const externalList: ImporterShoppingList[] = [];
+
+  for (const entry of list) {
+    const factoryItems = [];
+    const externalItems = [];
+
+    for (const item of entry.items) {
+      const ingredientName = item.name as IngredientName;
+      const { amountPerBox } = ingredients[ingredientName];
+
+      const rawFactoryAmount = item.amount * factoryRatio;
+
+      // factory rounded to full boxes (fit as much in the factory as possible)
+      const factoryBoxes = Math.ceil(rawFactoryAmount / amountPerBox);
+      const factoryAmount = factoryBoxes * amountPerBox;
+
+      // remainder goes to external
+      const remainingAmount = item.amount - factoryAmount;
+
+      factoryItems.push({
+        name: item.name,
+        amount: factoryAmount,
+        value: item.value * factoryRatio,
+      });
+
+      if (remainingAmount > 0) {
+        externalItems.push({
+          name: item.name,
+          amount: remainingAmount,
+          value: item.value * (1 - factoryRatio),
+        });
+      }
+    }
+
+    factoryList.push({ importer: entry.importer, items: factoryItems });
+
+    if (externalItems.length > 0) {
+      externalList.push({
+        importer: entry.importer,
+        items: externalItems,
+      });
+    }
+  }
+
+  return { factoryList, externalList };
+};
