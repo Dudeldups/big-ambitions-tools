2c8051e5ca06ca57340838cf5e8cd44f303c904d
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-20 21:54:54 +0200

    refactor active nav link styling

--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -1,24 +1,30 @@
 "use client";
 
-import { useSelectedLayoutSegment } from "next/navigation";
 import { ComponentProps } from "react";
-import { Link } from "@/i18n/navigation";
+import { Link, usePathname } from "@/i18n/navigation";
 import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
 
 export default function NavLink({
   href,
   ...rest
 }: ComponentProps<typeof Link>) {
-  const selectedLayoutSegment = useSelectedLayoutSegment();
-  const pathname = selectedLayoutSegment ? `/${selectedLayoutSegment}` : "/";
+  const pathname = usePathname();
   const isActive = pathname === href;
 
   return (
-    <Link
-      aria-current={isActive ? "page" : undefined}
-      href={href}
-      className={cn(isActive && "font-bold")}
-      {...rest}
-    />
+    <Button
+      asChild
+      className={cn(
+        "bg-muted/80 text-foreground border-border hover:bg-accent-foreground! hover:text-background border",
+        isActive && "bg-foreground/90 text-background",
+      )}
+    >
+      <Link
+        aria-current={isActive ? "page" : undefined}
+        href={href}
+        {...rest}
+      />
+    </Button>
   );
 }
