adcca228f4a9be4994ce3fffba7f0a212592170e
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-20 17:13:44 +0200

    add gradient

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -1,10 +1,16 @@
+import { cn } from "@/lib/utils";
 import NavLink from "./nav-link";
 import ThemeToggle from "./theme-toggle";
 
 const PageHeader = () => {
   return (
-    <header className="flex justify-center">
-      <div className="max-w-page bg-accent flex w-full items-center justify-between px-4 py-5">
+    <header className="flex justify-center py-4">
+      <div
+        className={cn(
+          "max-w-page border-border flex w-full items-center justify-between rounded-lg border px-4 py-3",
+          "from-card to-card via-accent/80 bg-linear-150 from-15% via-50% to-85%",
+        )}
+      >
         <nav>
           <ul className="flex flex-col gap-4 md:flex-row">
             <li>
