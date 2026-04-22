41fc93a2c7133bf7e70ab329c608465c9a3e6b28
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-22 23:55:39 +0200

    create glossary

---
+++ i18n/glossary.ts
@@ -0,0 +1,7 @@
+export const GLOSSARY = {
+  siteName: "Big Ambitions Tools",
+  gameName: "Big\u00A0Ambitions",
+  companyName: "Hovgaard\u00A0Games",
+  steam: "Steam",
+  gameVersion: "0.10",
+} as const;
