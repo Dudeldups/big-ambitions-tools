fbb7d2963fce82223e228f2492ec890073f3ad29
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-22 15:25:41 +0200

    add github link

--- components/page-footer.tsx
+++ components/page-footer.tsx
@@ -10,29 +10,41 @@ const PageFooter = () => {
   return (
     <footer
       className={cn(
-        "relative mt-14 grid place-items-center text-center md:mt-24",
+        "relative mt-14 grid place-items-center text-center text-pretty md:mt-24",
         "bg-card/50 backdrop-blur-sm",
-        "via-background/20 to-ring/10 from-ring/10 bg-linear-240",
+        "via-background to-ring/10 from-ring/10 bg-linear-240",
       )}
     >
       <span className="from-primary/10 via-primary/50 absolute inset-x-0 top-0 h-px bg-linear-to-r to-transparent" />
       <div className="max-w-page w-full p-6">
         <div className="text-muted-foreground space-y-2 pb-3 text-sm">
-          <p>{t("fanMade")}</p>
+          <p>
+            {t.rich("fanMade", {
+              link: (chunks) => (
+                <a
+                  href="https://github.com/Dudeldups/big-ambitions-tools"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="link"
+                >
+                  {chunks}
+                </a>
+              ),
+            })}
+          </p>
 
           <p>{t("assets")}</p>
         </div>
 
         <p className="font-medium">
           © {startYear}
-          {currentYear > startYear && ` – ${currentYear}`}
+          {currentYear > startYear && ` – ${currentYear}`}{" "}
           <a
             href="https://github.com/Dudeldups"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-primary dark:text-primary-light underline-offset-4 transition-colors hover:underline"
+            className="link"
           >
-            {" "}
             Dudeldups
           </a>
         </p>
