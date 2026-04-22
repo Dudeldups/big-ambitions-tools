ca7398e7ea1ce637e0745eb864f1797b64334694
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-22 17:57:57 +0200

    add SmartLink

--- components/page-footer.tsx
+++ components/page-footer.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { useTranslations } from "next-intl";
+import { SmartLink } from "./smart-link";
 
 const PageFooter = () => {
   const t = useTranslations("footer");
@@ -10,25 +11,18 @@ const PageFooter = () => {
   return (
     <footer
       className={cn(
-        "relative mt-14 grid place-items-center text-center text-pretty md:mt-24",
         "bg-card/50 backdrop-blur-sm",
         "via-background to-ring/10 from-ring/10 bg-linear-240",
       )}
     >
-      <span className="from-primary/10 via-primary/50 absolute inset-x-0 top-0 h-px bg-linear-to-r to-transparent" />
       <div className="max-w-page w-full p-6">
         <div className="text-muted-foreground space-y-2 pb-3 text-sm">
           <p>
             {t.rich("fanMade", {
               link: (chunks) => (
-                <a
-                  href="https://github.com/Dudeldups/big-ambitions-tools"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="link"
-                >
+                <SmartLink href="https://github.com/Dudeldups/big-ambitions-tools">
                   {chunks}
-                </a>
+                </SmartLink>
               ),
             })}
           </p>
@@ -38,15 +32,8 @@ const PageFooter = () => {
 
         <p className="font-medium">
           © {startYear}
-          {currentYear > startYear && ` – ${currentYear}`}{" "}
-          <a
-            href="https://github.com/Dudeldups"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link"
-          >
-            Dudeldups
-          </a>
+          {currentYear > startYear && ` - ${currentYear}`}{" "}
+          <SmartLink href="https://github.com/Dudeldups">Dudeldups</SmartLink>
         </p>
       </div>
     </footer>
