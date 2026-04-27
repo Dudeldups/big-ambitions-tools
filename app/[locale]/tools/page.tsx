b2fa6b2e9cea3545536f4b19c5602c2e38f08a87
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-27 15:48:13 +0200

    style tools page general layout

--- app/[locale]/tools/page.tsx
+++ app/[locale]/tools/page.tsx
@@ -1,33 +1,49 @@
-"use client";
-
+import DefaultHgroup from "@/components/deco/default-hgroup";
+import SectionSeparator from "@/components/deco/section-separator";
+import SectionWrapper from "@/components/deco/section-wrapper";
 import CreatePlaythroughForm from "@/components/tools/create-playthrough-form";
 import PlaythroughOverview from "@/components/tools/playthrough-overview";
+import { TriangleAlert } from "lucide-react";
 import { useTranslations } from "next-intl";
 
 const Tools = () => {
   const t = useTranslations("tools");
+  const tGeneral = useTranslations("general");
 
   return (
-    <>
-      <section className="max-w-page mx-auto w-full px-4">
-        <div className="bg-background border-border border-b">
-          <div className="mx-auto flex flex-wrap items-center gap-x-6 gap-y-3 px-4 py-3 sm:px-6">
-            <CreatePlaythroughForm />
+    <div className="main-wrapper">
+      <SectionWrapper variant="primary" centerMobile>
+        <hgroup className="grid items-end gap-14 md:grid-cols-[2fr_3fr] lg:gap-24">
+          <h1>{tGeneral("tools")}</h1>
+
+          <div className="max-w-lg">
+            <p className="text-h5">{t("intro.desc")}</p>
           </div>
+        </hgroup>
+      </SectionWrapper>
+
+      <SectionWrapper className="gap-10 md:gap-14">
+        <DefaultHgroup
+          title={tGeneral("playthroughs")}
+          caption={t("playthroughs.desc")}
+        />
+
+        <div className="bg-card shadow-accent border-destructive/50 mx-auto flex max-w-md flex-col items-center gap-4 rounded-lg border p-4 shadow-sm md:max-w-xl md:flex-row">
+          <div className="bg-destructive/10 text-foreground border-destructive/50 pointer-events-none rounded-md border p-1.5">
+            <TriangleAlert className="size-6" />
+          </div>
+          <p>{t("playthroughs.warning")}</p>
         </div>
 
-        <hgroup>
-          <h1>{t("intro.title")}</h1>
-          <p>{t("intro.desc")}</p>
-        </hgroup>
+        <SectionSeparator />
 
         <div>
-          <p>{t("intro.warning")}</p>
+          <CreatePlaythroughForm />
         </div>
 
         <PlaythroughOverview />
-      </section>
-    </>
+      </SectionWrapper>
+    </div>
   );
 };
 
