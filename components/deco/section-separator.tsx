a3e208eed8708ca5e6ab9cf6fb712c70ed08e517
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-27 15:41:41 +0200

    fix max width constraint

--- components/deco/section-separator.tsx
+++ components/deco/section-separator.tsx
@@ -2,7 +2,7 @@ import { Separator } from "../ui/separator";
 
 const SectionSeparator = () => {
   return (
-    <Separator className="via-foreground/20 max-w-page mx-auto bg-linear-to-r from-transparent to-transparent data-horizontal:w-[calc(100%-2*var(--spacing-clamp-x))]" />
+    <Separator className="via-foreground/20 max-w-page mx-auto bg-linear-to-r from-transparent to-transparent data-horizontal:w-[calc(100vw-2*var(--spacing-clamp-x))]" />
   );
 };
 
