5f9e635b35f7bfbe00ac9ba5f3c4e8b94fe0fd30
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-27 15:18:57 +0200

    fix width on smaller screens

--- components/deco/section-separator.tsx
+++ components/deco/section-separator.tsx
@@ -2,7 +2,7 @@ import { Separator } from "../ui/separator";
 
 const SectionSeparator = () => {
   return (
-    <Separator className="max-w-page via-foreground/20 mx-auto bg-linear-to-r from-transparent to-transparent" />
+    <Separator className="via-foreground/20 max-w-page mx-auto bg-linear-to-r from-transparent to-transparent data-horizontal:w-[calc(100%-2*var(--spacing-clamp-x))]" />
   );
 };
 
