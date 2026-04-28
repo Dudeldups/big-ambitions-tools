155df0eaff3db07e1c54f5fa20a4bd0a944f8e05
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-28 10:12:35 +0200

    add as prop

--- components/deco/default-hgroup.tsx
+++ components/deco/default-hgroup.tsx
@@ -4,13 +4,21 @@ type DefaultHgroupProps = {
   title: string | React.ReactNode;
   caption?: string | React.ReactNode;
   className?: string;
+  captionAs?: React.ElementType;
 };
 
-const DefaultHgroup = ({ title, caption, className }: DefaultHgroupProps) => {
+const DefaultHgroup = ({
+  title,
+  caption,
+  className,
+  captionAs: Component = "p",
+}: DefaultHgroupProps) => {
   return (
     <hgroup className={cn("max-w-2xl space-y-4", className)}>
       <h2>{title}</h2>
-      {caption && <p className="text-muted-foreground">{caption}</p>}
+      {caption && (
+        <Component className="text-muted-foreground">{caption}</Component>
+      )}
     </hgroup>
   );
 };
