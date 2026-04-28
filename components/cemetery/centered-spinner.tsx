7c003212d6961040fe695340c5c55f9890d559e5
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-28 21:58:56 +0200

    refactor padding

--- components/cemetery/centered-spinner.tsx
+++ components/cemetery/centered-spinner.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "../ui/spinner";
 
 const CenteredSpinner = () => {
   return (
-    <div className="grid place-items-center p-10">
+    <div className="grid place-items-center py-20">
       <Spinner className="size-6" />
     </div>
   );
