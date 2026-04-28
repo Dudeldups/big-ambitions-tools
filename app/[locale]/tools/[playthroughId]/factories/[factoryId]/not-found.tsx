f0282e35116e2fe9bee2117e6a1a2ce07000ba34
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-28 21:59:39 +0200

    fix 404 page shown on delete

--- app/[locale]/tools/[playthroughId]/factories/[factoryId]/not-found.tsx
+++ app/[locale]/tools/[playthroughId]/factories/[factoryId]/not-found.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import CenteredSpinner from "@/components/cemetery/centered-spinner";
 import NotFound from "@/components/not-found";
+import { usePathname } from "@/i18n/navigation";
 import { useActivePlaythrough } from "@/lib/hooks/useActivePlaythrough";
 import { useTranslations } from "next-intl";
 
 const FactoryNotFound = () => {
+  const pathname = usePathname();
   const { activePlaythrough } = useActivePlaythrough();
   const t = useTranslations("tools.factories.notFound");
 
   if (!activePlaythrough) return null;
+  if (pathname.includes(activePlaythrough.id)) return <CenteredSpinner />;
 
   return (
     <NotFound
