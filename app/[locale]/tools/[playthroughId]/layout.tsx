15e41dd0a37fa798972a9901ae241f7a8092c70f
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-28 09:07:48 +0200

    fix tw classes

--- app/[locale]/tools/[playthroughId]/layout.tsx
+++ app/[locale]/tools/[playthroughId]/layout.tsx
@@ -51,7 +51,7 @@ const PlaythroughIdLayout = ({ children }: { children: React.ReactNode }) => {
       <SectionWrapper variant="primary" size="compact" />
 
       <div className="px-clamp-x mt-10 mb-14">
-        <section className="bg-background shadow-foreground/30 border-foreground/20 mx-auto max-w-(--spacing-page) rounded-lg border px-4 py-3 shadow-sm">
+        <section className="bg-background shadow-foreground/30 border-foreground/20 max-w-page mx-auto rounded-lg border px-4 py-3 shadow-sm">
           <div className="flex flex-col justify-between gap-4 md:flex-row md:items-center">
             <div className="flex flex-col gap-4 xl:flex-row xl:items-center xl:gap-6">
               {/* Playthrough name */}
