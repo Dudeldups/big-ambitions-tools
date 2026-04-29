709003b7b7c32c174cdb542586f4f1149c6723d7
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-04-30 01:46:54 +0200

    add umami script

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -9,6 +9,7 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import PageHeader from "@/components/page-header";
 import PageFooter from "@/components/page-footer";
 import { cn } from "@/lib/utils";
+import Script from "next/script";
 
 const rubikSans = Rubik({
   variable: "--font-rubik-sans",
@@ -44,6 +45,14 @@ export default async function RootLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning>
+      <Script
+        defer
+        src="https://analytics.big-ambitions-tools.com/script.js"
+        data-website-id="9201b0ca-c66d-46fc-b24a-adf44dc44b16"
+        data-domains="big-ambitions-tools.com,www.big-ambitions-tools.com"
+        strategy="afterInteractive"
+      />
+
       <body
         className={cn(
           "flex min-h-screen flex-col antialiased",
