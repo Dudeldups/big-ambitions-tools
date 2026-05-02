47bbaad4b97e76f39df0cfd48372bdc0079098c4
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-05-02 12:15:54 +0200

    add footer links for legal and privacy

--- components/page-footer.tsx
+++ components/page-footer.tsx
@@ -1,9 +1,10 @@
 import { cn } from "@/lib/utils";
 import { useTranslations } from "next-intl";
 import { SmartLink } from "./smart-link";
+import { Separator } from "./ui/separator";
 
 const PageFooter = () => {
-  const t = useTranslations("footer");
+  const t = useTranslations("");
 
   const startYear = 2026;
   const currentYear = new Date().getFullYear();
@@ -16,26 +17,58 @@ const PageFooter = () => {
         "via-background to-ring/10 from-ring/10 bg-linear-240",
       )}
     >
+      {/* bg-line */}
       <span className="from-primary/20 via-primary/50 to-primary/20 absolute inset-x-0 top-0 h-px bg-linear-to-r" />
-      <div className="max-w-page w-full py-6">
-        <div className="text-muted-foreground space-y-2 pb-3 text-sm">
+
+      {/* inner footer */}
+      <div className="max-w-page w-full space-y-3 py-6">
+        <div className="mx-auto grid max-w-max gap-3 text-sm max-sm:mb-5 sm:grid-cols-[1fr_auto_1fr]">
+          <SmartLink
+            href="/privacy"
+            className="justify-self-end hover:underline"
+          >
+            {t("legal.link")}
+          </SmartLink>
+
+          <Separator
+            orientation="vertical"
+            className="bg-foreground/20 mx-3 hidden sm:block"
+          />
+
+          <SmartLink
+            href="/legal"
+            className="justify-self-start hover:underline"
+          >
+            {t("privacy.link")}
+          </SmartLink>
+        </div>
+
+        <div className="text-muted-foreground space-y-2 text-sm">
           <p className="text-pretty">
-            {t.rich("fanMade", {
+            {t.rich("footer.fanMade", {
               link: (chunks) => (
-                <SmartLink href="https://github.com/Dudeldups/big-ambitions-tools">
+                <SmartLink
+                  href="https://github.com/Dudeldups/big-ambitions-tools"
+                  className="hover:underline"
+                >
                   {chunks}
                 </SmartLink>
               ),
             })}
           </p>
 
-          <p className="text-pretty">{t("assets")}</p>
+          <p className="text-pretty">{t("footer.assets")}</p>
         </div>
 
         <p className="font-medium">
           © {startYear}
           {currentYear > startYear && ` - ${currentYear}`}{" "}
-          <SmartLink href="https://github.com/Dudeldups">Dudeldups</SmartLink>
+          <SmartLink
+            href="https://github.com/Dudeldups"
+            className="hover:underline"
+          >
+            Dudeldups
+          </SmartLink>
         </p>
       </div>
     </footer>
