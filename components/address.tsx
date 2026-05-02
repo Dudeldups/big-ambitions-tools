6bbc85e54d9ad74755e181efeefbcdd3eb9ce700
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-05-02 13:08:36 +0200

    add address component

---
+++ components/address.tsx
@@ -0,0 +1,27 @@
+const name = process.env.NEXT_PUBLIC_LEGAL_NAME;
+const line1 = process.env.NEXT_PUBLIC_LEGAL_ADDRESS_LINE1;
+const line2 = process.env.NEXT_PUBLIC_LEGAL_ADDRESS_LINE2;
+const city = process.env.NEXT_PUBLIC_LEGAL_ADDRESS_CITY;
+const email = process.env.NEXT_PUBLIC_LEGAL_EMAIL;
+
+const Address = () => {
+  return (
+    <address className="space-y-1 not-italic">
+      {name && <p>{name}</p>}
+      {line1 && <p>{line1}</p>}
+      {line2 && <p>{line2}</p>}
+      {city && <p>{city}</p>}
+
+      {email && (
+        <p className="pt-2">
+          E-Mail:{" "}
+          <a href={`mailto:${email}`} className="underline hover:no-underline">
+            {email}
+          </a>
+        </p>
+      )}
+    </address>
+  );
+};
+
+export default Address;
