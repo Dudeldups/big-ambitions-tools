de35f43fb07ca779f962516fa51a0b81227d8288
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-05-02 10:35:07 +0200

    create siteConstants

--- lib/siteConstants.ts
+++ lib/siteConstants.ts
@@ -1,2 +1,16 @@
 export const BASE_URL = "https://big-ambitions-tools.com";
 export const DEFAULT_LOCALE = "en";
+
+export const ROUTES = [
+  "",
+  "/about",
+  "/database",
+  "/database/ingredients",
+  "/database/inventory",
+  "/database/machines",
+  "/database/products",
+  "/database/vehicles",
+  "/legal",
+  "/privacy",
+  "/tools",
+];
