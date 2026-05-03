ae7ddbf3e824109fd9390810f94024e9dbe41b8a
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-05-03 15:43:00 +0200

    add card background and icon

--- app/[locale]/legal/page.tsx
+++ app/[locale]/legal/page.tsx
@@ -1,5 +1,7 @@
 import Address from "@/components/address";
+import ImageCircleBg from "@/components/deco/image-circle-bg";
 import SectionWrapper from "@/components/deco/section-wrapper";
+import { Scale } from "lucide-react";
 import { useTranslations } from "next-intl";
 
 const LegalPage = () => {
@@ -11,8 +13,12 @@ const LegalPage = () => {
         <h1>{t("title")}</h1>
       </SectionWrapper>
 
-      <SectionWrapper>
-        <div className="space-y-4">
+      <SectionWrapper className="items-center md:items-start">
+        <ImageCircleBg size="2xl" variant="accent">
+          <Scale />
+        </ImageCircleBg>
+
+        <div className="bg-card border-border shadow-accent mt-8 max-w-max space-y-4 rounded-xl border p-8 shadow-md">
           {t.has("subtitle") && (
             <p className="text-lg font-semibold">{t("subtitle")}</p>
           )}
