59b792b6965b6b0d1b85b44dde29587122a05a54
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-05-03 15:35:24 +0200

    fix link contrast

--- components/address.tsx
+++ components/address.tsx
@@ -1,3 +1,5 @@
+import { Button } from "./ui/button";
+
 const name = process.env.NEXT_PUBLIC_LEGAL_NAME;
 const line1 = process.env.NEXT_PUBLIC_LEGAL_ADDRESS_LINE1;
 const line2 = process.env.NEXT_PUBLIC_LEGAL_ADDRESS_LINE2;
@@ -15,9 +17,11 @@ const Address = () => {
       {email && (
         <p className="pt-2">
           E-Mail:{" "}
-          <a href={`mailto:${email}`} className="underline hover:no-underline">
-            {email}
-          </a>
+          <Button asChild>
+            <a href={`mailto:${email}`} className="font-semibold">
+              {email}
+            </a>
+          </Button>
         </p>
       )}
     </address>
