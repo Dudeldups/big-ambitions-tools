b7097eecdd7a72b8deba7bd68366cf46aa06d225
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-05-25 23:43:21 +0200

    add 0.11 dummy data

--- lib/game/registry.ts
+++ lib/game/registry.ts
@@ -1,9 +1,11 @@
 import { gameData as gameData010 } from "@/data/game/0.10";
+import { gameData as gameData011 } from "@/data/game/0.11";
 import { GameData } from "./types";
 import { GameVersion } from "./versions";
 
 export const GAME_DATA_BY_VERSION: Partial<Record<GameVersion, GameData>> = {
   "0.10": gameData010,
+  "0.11": gameData011,
 };
 
 export const getGameData = (version: GameVersion): GameData => {
