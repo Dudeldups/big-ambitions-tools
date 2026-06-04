63446cf4c435f334af2d0feb1f70284257623217
Arne Jacob <130840918+Dudeldups@users.noreply.github.com>
2026-06-04 22:24:16 +0200

    update updateHistory

--- lib/updateHistory.ts
+++ lib/updateHistory.ts
@@ -7,4 +7,12 @@ export const updateHistory: { id: UpdateId; date: string }[] = [
     id: "launch",
     date: "2026-05-03",
   },
+  {
+    id: "qotd",
+    date: "2026-06-04",
+  },
+  {
+    id: "update11",
+    date: "2026-06-04",
+  },
 ];
